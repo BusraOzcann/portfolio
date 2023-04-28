23ee5c03baaaca3764c8d8aceb3cbe9164622830
BusraOzcann <ozcan.busra1999@gmail.com>
2023-04-28 22:53:28 +0300

    süreçler güncellendi

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -2,46 +2,6 @@
     <div id="experience" class="container py-5" style="height: 500px; ">
         <div style="margin-top: 60px">
             
-            <!-- <v-timeline align="start">
-
-                <v-timeline-item>
-                <template v-slot:opposite>
-                    2016 Eylül - 2021 Mart
-                </template>
-                <div>
-                    <div class="text-h6">Üniversite</div>
-                    <p>
-                        Cumhuriyet Üniversitesi Bilgisayar Mühendisliği
-                    </p>
-                </div>
-                </v-timeline-item>
-
-                <v-timeline-item>
-                <template v-slot:opposite>
-                    2021 Şubat - 2022 Mart
-                </template>
-                <div>
-                    <div class="text-h6">İş Tecrübesi</div>
-                    <p>
-                        Sivas Teknokent Hakan Bilgi Teknolojileri
-                    </p>
-                </div>
-                </v-timeline-item>
-
-                <v-timeline-item>
-                <template v-slot:opposite>
-                    2021 Eylül - ~
-                </template>
-                <div>
-                    <div class="text-h6">Yüksek Lisans</div>
-                    <p>
-                        Sivas Bilim ve Teknoloji Üniversitesi Savunma teknolojileri A.B.D
-                    </p>
-                </div>
-                </v-timeline-item>
-            </v-timeline> -->
-
-
              <v-timeline align="start">
                 <v-timeline-item
                     v-for="(year, i) in years"
@@ -72,9 +32,9 @@ import {ref} from "vue"
 export default {
     setup(){
         const years = ref([
-            { year : "2016 Eylül - 2021 Mart", color: "#7827cf", title: "Üniversite", text: "Cumhuriyet Üniversitesi Bilgisayar Mühendisliği"},
-            { year : "2021 Eylül - ~", color: "#8739da", title: "Yüksek Lisans", text: "Sivas Bilim ve Teknoloji Üniversitesi Savunma Teknolojileri A.B.D"},
-            { year: "2022 Şubat - 2022 Mart", color: "#9d6ad4", title: "İş Tecrübesi", text: "Sivas Teknokent Hakan Bilgi Teknolojileri" }
+            { year : "2016 Eylül - 2021 Mart", color: "#fcb404", title: "Üniversite", text: "Cumhuriyet Üniversitesi Bilgisayar Mühendisliği"},
+            { year : "2021 Eylül - ~", color: "#fcbc36", title: "Yüksek Lisans", text: "Sivas Bilim ve Teknoloji Üniversitesi Savunma Teknolojileri A.B.D"},
+            { year: "2022 Şubat - 2022 Mart", color: "#fcc660", title: "İş Tecrübesi", text: "Sivas Teknokent Hakan Bilgi Teknolojileri" }
         ])
 
         return {years}
